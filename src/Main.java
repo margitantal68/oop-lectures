62892c63631f6b986f27227f9f715ff7b15def7b
Margit Antal <manyi@ms.sapientia.ro>
2021-02-22 15:33:07 +0200

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Lecture 2.");
-        TestClass instance1 = new TestClass();
-        System.out.println( instance1.toString() );
+        System.out.println("Project main - Do nothing");
     }
 }
